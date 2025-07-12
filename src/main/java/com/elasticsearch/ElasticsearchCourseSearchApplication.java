e41abfed556b55cce05d9ce456f98b989918d31a
Abhijeet Singh <abhijeetcs4725@gmail.com>
2025-07-12 14:45:22 +0530

    Add project structure and initial setup

---
+++ src/main/java/com/elasticsearch/ElasticsearchCourseSearchApplication.java
@@ -0,0 +1,13 @@
+package com.elasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticsearchCourseSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticsearchCourseSearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elasticsearch/ElasticsearchCourseSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elasticsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticsearchCourseSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
